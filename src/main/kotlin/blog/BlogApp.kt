52ab3d6df96c3ec5bb8b106d06f104bd1489fa94
meshnesh <tonnie.nesh@gmail.com>
2020-05-06 14:33:39 +0300

    [Ft] First project setup

---
+++ src/main/kotlin/blog/BlogApp.kt
@@ -0,0 +1,20 @@
+package blog
+
+import io.ktor.application.*
+import io.ktor.features.CallLogging
+import io.ktor.features.DefaultHeaders
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun Application.main() {
+    install(DefaultHeaders)
+    install(CallLogging)
+    install(Routing) {
+        get("/") {
+            call.respondText("My Example Blog2", ContentType.Text.Html)
+        }
+    }
+}
